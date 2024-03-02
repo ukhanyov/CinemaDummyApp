aeb86b333cacfaf9c8d25e2577e0f0c0bccaf222
Oleg <ukhanyov.oleg@gmail.com>
2024-03-02 16:52:38 +0200

    ConfirmCreateAccount start

---
+++ app/src/main/java/com/example/cinemadummyapp/screens/ConfirmCreateAccountScreen.kt
@@ -0,0 +1,17 @@
+package com.example.cinemadummyapp.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cinemadummyapp.ui.theme.CinemaDummyAppTheme
+
+@Preview
+@Composable
+fun ConfirmCreateAccountScreenPreview() {
+    CinemaDummyAppTheme {
+        ConfirmCreateAccountScreen()
+    }
+}
+
+@Composable
+fun ConfirmCreateAccountScreen() {
+}
