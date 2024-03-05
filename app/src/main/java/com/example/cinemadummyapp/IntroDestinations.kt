0ea59fae1ca634ad0753d371eb45f91c5d2dce54
Oleg <ukhanyov.oleg@gmail.com>
2024-03-05 14:08:33 +0200

    navigation refactoring

--- app/src/main/java/com/example/cinemadummyapp/IntroDestinations.kt
+++ app/src/main/java/com/example/cinemadummyapp/IntroDestinations.kt
@@ -2,27 +2,27 @@ package com.example.cinemadummyapp
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
-interface CinemaDestination {
+interface IntroDestination {
     val icon: ImageVector?
     val route: String
 }
 
-object Onboarding : CinemaDestination {
+object Onboarding : IntroDestination {
     override val icon = null
     override val route = "onboarding"
 }
 
-object CreateAccount : CinemaDestination {
+object CreateAccount : IntroDestination {
     override val icon = null
     override val route = "create_account"
 }
 
-object ConfirmCreateAccount : CinemaDestination {
+object ConfirmCreateAccount : IntroDestination {
     override val icon = null
     override val route = "confirm_create_account"
 }
 
-object Login : CinemaDestination {
+object Login : IntroDestination {
     override val icon = null
     override val route = "login"
 }
