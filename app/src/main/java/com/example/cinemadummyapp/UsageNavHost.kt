763222f5a84cc3afb009ea384fa52856391132b6
Oleg <ukhanyov.oleg@gmail.com>
2024-03-06 12:43:55 +0200

    app usage start

--- app/src/main/java/com/example/cinemadummyapp/UsageNavHost.kt
+++ app/src/main/java/com/example/cinemadummyapp/UsageNavHost.kt
@@ -1,5 +1,6 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -7,14 +8,13 @@ import com.example.cinemadummyapp.ConfirmCreateAccount
 import com.example.cinemadummyapp.CreateAccount
 import com.example.cinemadummyapp.Login
 import com.example.cinemadummyapp.Onboarding
-import com.example.cinemadummyapp.common.navigateSingleTopTo
 import com.example.cinemadummyapp.screens.ConfirmCreateAccountScreen
 import com.example.cinemadummyapp.screens.CreateAccountScreen
 import com.example.cinemadummyapp.screens.LoginScreen
 import com.example.cinemadummyapp.screens.OnboardingScreen
 
 @Composable
-fun CinemaNavHost(
+fun UsageNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
@@ -23,28 +23,15 @@ fun CinemaNavHost(
         startDestination = Onboarding.route,
         modifier = modifier
     ) {
-        composable(route = Onboarding.route) {
-            OnboardingScreen(
-                goToCreateAccount = {
-                    navController.navigateSingleTopTo(CreateAccount.route)
-                },
-                goToLogin = {
-                    navController.navigateSingleTopTo(Login.route)
-                }
-            )
-        }
-        composable(route = CreateAccount.route) {
-            CreateAccountScreen(
-                onCreateAccountClicked = {
-                    navController.navigateSingleTopTo(ConfirmCreateAccount.route)
-                }
-            )
-        }
-        composable(route = ConfirmCreateAccount.route) {
-            ConfirmCreateAccountScreen()
-        }
-        composable(route = Login.route) {
-            LoginScreen()
-        }
+//        composable(route = Onboarding.route) {
+//            OnboardingScreen(
+//                goToCreateAccount = {
+//                    navController.navigateSingleTopTo(CreateAccount.route)
+//                },
+//                goToLogin = {
+//                    navController.navigateSingleTopTo(Login.route)
+//                }
+//            )
+//        }
     }
 }
---
+++ app/src/main/java/com/example/cinemadummyapp/screens/UsageScreen.kt
@@ -0,0 +1,2 @@
+package com.example.cinemadummyapp.screens
+
