c4f53609583cb618f253d770cac4907dbd7cb439
Oleg <ukhanyov.oleg@gmail.com>
2024-03-23 13:20:24 +0200

    theater seats, seats display done

--- app/src/main/java/com/example/cinemadummyapp/interaction/InteractionNavHost.kt
+++ app/src/main/java/com/example/cinemadummyapp/interaction/InteractionNavHost.kt
@@ -1,6 +1,5 @@
 package com.example.cinemadummyapp.interaction
 
-import android.util.Log
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
--- app/src/main/java/com/example/cinemadummyapp/interaction/theater_seats/TheaterSeats.kt
+++ app/src/main/java/com/example/cinemadummyapp/interaction/theater_seats/TheaterSeats.kt
@@ -1,9 +1,8 @@
 package com.example.cinemadummyapp.interaction.theater_seats
 
+import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -12,10 +11,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import com.example.cinemadummyapp.R
 import com.example.cinemadummyapp.common.movies.Movie
 import com.example.cinemadummyapp.common.movies.randomMovie
 import com.example.cinemadummyapp.common.tickets.TicketState
@@ -49,6 +55,14 @@ fun TheaterSeatsScreen(
     onBackClicked: () -> Unit = {},
     movie: Movie,
 ) {
+
+    val activity = LocalView.current.context as Activity
+    activity.window.statusBarColor = Color.White.toArgb()
+    WindowCompat.getInsetsController(
+        activity.window,
+        LocalView.current
+    ).isAppearanceLightStatusBars = true
+
     var isButtonEnabled by remember { mutableStateOf(false) }
     var ticketsGrid by remember { mutableStateOf(makeTicketsGrid(movie)) }
     LaunchedEffect(ticketsGrid) {
@@ -63,11 +77,57 @@ fun TheaterSeatsScreen(
             toolbarState = ToolbarState.TheaterSeats(),
             onBackClicked = { onBackClicked() }
         )
-        LazyVerticalGrid(columns = GridCells.Fixed(6)) {
-            items(ticketsGrid, key = { it.id }) {
-
+        Column {
+            ticketsGrid.groupBy { it.row }.forEach { (row, tickets) ->
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .size(36.dp)
+                            .padding(top = 6.dp),
+                        text = "$row",
+                        fontSize = 24.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    tickets.forEach { ticket ->
+                        Image(
+                            modifier = Modifier.size(36.dp),
+                            imageVector = ImageVector.vectorResource(id = R.drawable.seat),
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+            Row {
+                Spacer(modifier = Modifier.size(36.dp))
+                ticketsGrid.map { it.seat }.toSet().forEach {
+                    Text(
+                        modifier = Modifier
+                            .size(36.dp),
+                        text = it,
+                        fontSize = 24.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
+//        LazyVerticalGrid(
+//            modifier = Modifier.sizeIn(maxWidth = (19.dp * 6 + (4.dp * 6))),
+//            verticalArrangement = Arrangement.spacedBy(4.dp), // Adjust spacing as needed
+//            horizontalArrangement = Arrangement.Center,
+//            columns = GridCells.Fixed(6)
+//        ) {
+//            items(ticketsGrid, key = { it.id }) {
+//                Image(
+//                    modifier = Modifier,
+//                    imageVector = ImageVector.vectorResource(id = R.drawable.seat),
+//                    contentDescription = null
+//                )
+//            }
+//        }
         Button(
             modifier = Modifier
                 .fillMaxWidth()
