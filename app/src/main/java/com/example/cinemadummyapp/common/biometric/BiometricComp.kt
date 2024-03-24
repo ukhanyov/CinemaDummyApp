d8a3f537ee4e2872e61fbaab27f780f82f743375
Oleg <ukhanyov.oleg@gmail.com>
2024-03-24 12:20:06 +0200

    onboarding, refactoring progress

---
+++ app/src/main/java/com/example/cinemadummyapp/common/biometric/BiometricComp.kt
@@ -0,0 +1,104 @@
+package com.example.cinemadummyapp.common.biometric
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.dp
+import com.example.cinemadummyapp.R
+import com.example.cinemadummyapp.ui.theme.CinemaDummyAppTheme
+
+@Preview
+@PreviewScreenSizes
+@PreviewDynamicColors
+@PreviewFontScale
+@PreviewLightDark
+@Composable
+fun BiometricPreview() {
+    CinemaDummyAppTheme {
+        val activity = LocalView.current.context as Activity
+        Biometric(BiometricPromptManager(activity as AppCompatActivity))
+    }
+}
+
+@Composable
+fun Biometric(
+    promptManager: BiometricPromptManager,
+    success: () -> Unit = {},
+) {
+    Box {
+        val activity = LocalView.current.context as Activity
+        val biometricResult by promptManager.promptResults.collectAsState(initial = null)
+        val enrollActivityResult = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.StartActivityForResult(),
+            onResult = { Toast.makeText(activity, "$it", Toast.LENGTH_SHORT).show() }
+        )
+        LaunchedEffect(biometricResult) {
+            if (biometricResult is BiometricPromptManager.BiometricResult.AuthenticationNotSet) {
+                if (Build.VERSION.SDK_INT >= 30) {
+                    val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                        putExtra(
+                            Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                            BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+                        )
+                    }
+                    enrollActivityResult.launch(enrollIntent)
+                }
+            }
+        }
+        Icon(
+            modifier = Modifier
+                .size(120.dp)
+                .clickable { promptManager.showBiometricPrompt("Login", "via biometrics") },
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_biometric),
+            tint = Color.White,
+            contentDescription = null
+        )
+        biometricResult?.let { result ->
+            when (result) {
+                is BiometricPromptManager.BiometricResult.AuthenticationError -> {
+                    Toast.makeText(activity, "AuthenticationError", Toast.LENGTH_SHORT).show()
+                }
+
+                BiometricPromptManager.BiometricResult.AuthenticationFailed -> {
+                    Toast.makeText(activity, "AuthenticationFailed", Toast.LENGTH_SHORT).show()
+                }
+
+                BiometricPromptManager.BiometricResult.AuthenticationNotSet -> {
+                    Toast.makeText(activity, "AuthenticationNotSet", Toast.LENGTH_SHORT).show()
+                }
+
+                BiometricPromptManager.BiometricResult.AuthenticationSuccess -> {
+                    success()
+                }
+
+                BiometricPromptManager.BiometricResult.FeatureUnavailable -> {
+                    Toast.makeText(activity, "FeatureUnavailable", Toast.LENGTH_SHORT).show()
+                }
+
+                BiometricPromptManager.BiometricResult.HardwareUnavailable -> {
+                    Toast.makeText(activity, "HardwareUnavailable", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/cinemadummyapp/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/example/cinemadummyapp/onboarding/OnboardingScreen.kt
@@ -1,43 +1,29 @@
 package com.example.cinemadummyapp.onboarding
 
 import android.app.Activity
-import android.content.Intent
-import android.os.Build
-import android.provider.Settings.ACTION_BIOMETRIC_ENROLL
-import android.provider.Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.biometric.BiometricManager.Authenticators
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.example.cinemadummyapp.R
+import com.example.cinemadummyapp.common.biometric.Biometric
 import com.example.cinemadummyapp.common.biometric.BiometricPromptManager
-import com.example.cinemadummyapp.common.biometric.BiometricPromptManager.BiometricResult
 import com.example.cinemadummyapp.ui.theme.AppMainAccent
 
 //@Preview
@@ -100,6 +86,7 @@ fun OnboardingScreen(
             ) {
                 OutlinedButton(
                     modifier = Modifier
+                        .fillMaxWidth()
                         .padding(horizontal = 48.dp),
                     shape = RoundedCornerShape(10.dp),
                     border = BorderStroke(width = 1.dp, color = Color.White),
@@ -125,73 +112,10 @@ fun OnboardingScreen(
                 )
             }
 
-            val biometricResult by promptManager.promptResults.collectAsState(initial = null)
-            val enrollActivityResult = rememberLauncherForActivityResult(
-                contract = ActivityResultContracts.StartActivityForResult(),
-                onResult = {
-                    Toast.makeText(activity, "${it}", Toast.LENGTH_LONG)
-                        .show()
-                }
+            Biometric(
+                promptManager = promptManager,
+                success = { goToUsage() }
             )
-            LaunchedEffect(biometricResult) {
-                if (biometricResult is BiometricResult.AuthenticationNotSet) {
-                    if (Build.VERSION.SDK_INT >= 30) {
-                        val enrollIntent = Intent(ACTION_BIOMETRIC_ENROLL).apply {
-                            putExtra(
-                                EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                                Authenticators.BIOMETRIC_STRONG or Authenticators.DEVICE_CREDENTIAL
-                            )
-                        }
-                        enrollActivityResult.launch(enrollIntent)
-                    }
-                }
-            }
-            Icon(
-                modifier = Modifier
-                    .size(width = 120.dp, height = 120.dp)
-                    .align(Alignment.BottomCenter)
-                    .padding(25.dp)
-                    .clickable {
-                        promptManager.showBiometricPrompt("Login", "via biometrics")
-                    },
-                imageVector = ImageVector.vectorResource(id = R.drawable.ic_biometric),
-                tint = Color.White,
-                contentDescription = null
-            )
-            biometricResult?.let { result ->
-                when (result) {
-                    is BiometricResult.AuthenticationError -> {
-                        Toast.makeText(activity, result.error, Toast.LENGTH_LONG).show()
-                    }
-
-                    BiometricResult.AuthenticationFailed -> {
-                        Toast.makeText(activity, "AuthenticationFailed", Toast.LENGTH_LONG)
-                            .show()
-                    }
-
-                    BiometricResult.AuthenticationNotSet -> {
-                        Toast.makeText(activity, "AuthenticationNotSet", Toast.LENGTH_LONG)
-                            .show()
-                    }
-
-                    BiometricResult.AuthenticationSuccess -> {
-                        Toast.makeText(activity, "AuthenticationSuccess", Toast.LENGTH_LONG)
-                            .show()
-                        goToUsage()
-                    }
-
-                    BiometricResult.FeatureUnavailable -> {
-                        Toast.makeText(activity, "FeatureUnavailable", Toast.LENGTH_LONG)
-                            .show()
-                    }
-
-                    BiometricResult.HardwareUnavailable -> {
-                        Toast.makeText(activity, "HardwareUnavailable", Toast.LENGTH_LONG)
-                            .show()
-                    }
-                }
-            }
-
         }
     }
 }
