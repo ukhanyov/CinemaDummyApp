378acb24c53d12d88691b8cc61550557d7e45741
Oleg <ukhanyov.oleg@gmail.com>
2024-03-25 13:09:50 +0200

    viewmodel dependency added + lib versions updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidyoutubeplayer)
+    implementation(libs.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
