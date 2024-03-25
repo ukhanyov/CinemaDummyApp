3c6a25d618c24af75df5ec5bd06fec4b8d632152
Oleg <ukhanyov.oleg@gmail.com>
2024-03-25 15:44:28 +0200

    main viewmodel body done

--- app/src/main/java/com/example/cinemadummyapp/MainViewModel.kt
+++ app/src/main/java/com/example/cinemadummyapp/MainViewModel.kt
@@ -1,13 +1,29 @@
 package com.example.cinemadummyapp
 
 import androidx.lifecycle.ViewModel
+import com.example.cinemadummyapp.common.tickets.Ticket
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
 
 class MainViewModel : ViewModel() {
 
-//    // Game UI state
-//    private val _uiState = MutableStateFlow(GameUiState())
-//    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private val _boughtTickets = MutableStateFlow<List<Ticket>>(emptyList())
+    val boughtTickets: StateFlow<List<Ticket>> = _boughtTickets.asStateFlow()
+
+    private val _cartTickets = MutableStateFlow<List<Ticket>>(emptyList())
+    val cartTickets: StateFlow<List<Ticket>> = _cartTickets.asStateFlow()
+
+    fun addToCart(newTickets: List<Ticket>) {
+        _cartTickets.getAndUpdate { it.toMutableList().apply { addAll(newTickets) } }
+    }
+
+    fun checkoutSuccess() {
+        cartTickets.value.let { tickets ->
+            if (tickets.isEmpty()) return
+            _boughtTickets.getAndUpdate { it.toMutableList().apply { addAll(tickets) } }
+        }
+    }
 
 }
