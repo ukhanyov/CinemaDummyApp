69a10cbe2df3ee3cfa8294c0ac7541f1904f893b
Oleg <ukhanyov.oleg@gmail.com>
2024-03-29 13:25:43 +0200

    payment screen, new card, card number edit progress

--- app/src/main/java/com/example/cinemadummyapp/interaction/payment/PaymentAddNewCardComp.kt
+++ app/src/main/java/com/example/cinemadummyapp/interaction/payment/PaymentAddNewCardComp.kt
@@ -1,10 +1,14 @@
 package com.example.cinemadummyapp.interaction.payment
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -15,6 +19,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -83,7 +88,7 @@ fun PaymentAddNewCard(
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceEvenly
+                        horizontalArrangement = Arrangement.End
                     ) {
                         cardNumber.split(" ").forEach {
                             Text(
@@ -121,7 +126,55 @@ fun PaymentAddNewCard(
                 }
             }
         }
-        Box(modifier = Modifier.weight(1f)) {}
+        Box(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    value = newCard?.cardNumber ?: "",
+                    onValueChange = {
+                        if (it.isNotEmpty() && it.last().toString() == " ") {
+                            newCard = newCard?.copy(cardNumber = it.trim())
+                        } else if (it.isNotEmpty() && it.last().isDigit()) {
+                            Log.d("my_tag", "start -> $it")
+                            val inputText = it.replace(" ", "")
+                            Log.d("my_tag", "inputText -> $inputText")
+                            if (inputText.length <= 16) {
+                                Log.d(
+                                    "my_tag",
+                                    "formatCardNumber -> ${formatCardNumber(inputText)}"
+                                )
+                                newCard = newCard?.copy(cardNumber = formatCardNumber(inputText))
+                            }
+                            Log.d("my_tag", "----------------")
+                        } else if (it.isBlank() || it.isEmpty()) {
+                            newCard = newCard?.copy(cardNumber = "")
+                        }
+                    },
+                    label = { Text("Card number") },
+                    placeholder = {
+                        Text(
+                            text = "xxxx-xxxx-xxxx-2024",
+                            color = Color.LightGray
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        disabledTextColor = Color.Black,
+                        disabledSupportingTextColor = Color.Black,
+                        errorTextColor = Color.Red,
+                        errorSupportingTextColor = Color.Red,
+                        focusedTextColor = Color.Black,
+                        focusedSupportingTextColor = Color.Black,
+                        unfocusedSupportingTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
+                    ),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                )
+            }
+        }
         Row {
             Spacer(modifier = Modifier.size(16.dp))
             Button(
@@ -159,4 +212,11 @@ fun PaymentAddNewCard(
             Spacer(modifier = Modifier.size(16.dp))
         }
     }
+}
+
+private fun formatCardNumber(inputString: String): String {
+    // Ensure the initial string is 16 characters long
+//    if (inputString.length != 16) return inputString
+
+    return inputString.chunked(4).joinToString(" ")
 }
