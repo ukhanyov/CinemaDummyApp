e38c81af0c1ffc1f744948cd13f198f674c3d479
Oleg <ukhanyov.oleg@gmail.com>
2024-03-29 12:49:55 +0200

    payment screen, new card display fields added

--- app/src/main/java/com/example/cinemadummyapp/interaction/payment/PaymentAddNewCardComp.kt
+++ app/src/main/java/com/example/cinemadummyapp/interaction/payment/PaymentAddNewCardComp.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,7 +28,17 @@ fun PaymentAddNewCard(
     returnToCardSelect: () -> Unit = {}
 ) {
     val cards by mainViewModel.cards.collectAsStateWithLifecycle()
-    var newCard by remember { mutableStateOf<Card?>(null) }
+    var newCard by remember {
+        mutableStateOf<Card?>(
+            Card(
+                id = "Add card",
+                cardNumber = "1234 4321 1234 4321",
+                cardDate = "31/09",
+                cardHolderName = "Your Name",
+                cardType = CardType.Mastercard,
+            )
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -49,6 +60,66 @@ fun PaymentAddNewCard(
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
             )
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize()
+                    .aspectRatio(154.dp / 85.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.Start,
+            ) {
+//                val imageVector = when (card.cardType) {
+//                    CardType.Visa -> ImageVector.vectorResource(id = R.drawable.ic_card_visa)
+//                    CardType.Mastercard -> ImageVector.vectorResource(id = R.drawable.ic_card_master)
+//                }
+                Image(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(64.dp),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_card_master),
+                    contentDescription = null,
+                )
+                newCard?.cardNumber?.let { cardNumber ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        cardNumber.split(" ").forEach {
+                            Text(
+                                text = it,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround,
+                ) {
+                    Column {
+                        Text(
+                            text = "CARD HOLDER",
+                            color = Color.White
+                        )
+                        Text(
+                            text = "Name",
+                            color = Color.White
+                        )
+                    }
+                    Column {
+                        Text(
+                            text = "EXPIRES",
+                            color = Color.White
+                        )
+                        Text(
+                            text = "31/9",
+                            color = Color.White
+                        )
+                    }
+                }
+            }
         }
         Box(modifier = Modifier.weight(1f)) {}
         Row {
