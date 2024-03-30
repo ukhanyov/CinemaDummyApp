b04309247462ae9869d532ff5c7a6c30dba6858f
Oleg <ukhanyov.oleg@gmail.com>
2024-03-30 11:11:22 +0200

    payment screen, payment progress comp, done

--- app/src/main/java/com/example/cinemadummyapp/interaction/payment/PaymentProgressComp.kt
+++ app/src/main/java/com/example/cinemadummyapp/interaction/payment/PaymentProgressComp.kt
@@ -1,7 +1,17 @@
 package com.example.cinemadummyapp.interaction.payment
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cinemadummyapp.ui.theme.CinemaDummyAppTheme
 
 @Preview
@@ -18,5 +28,27 @@ fun OnboardingScreenPreview() {
 
 @Composable
 fun PaymentProgressComp() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CircularProgressIndicator()
+        Spacer(modifier = Modifier.size(16.dp))
+        Text(
+            text = "Almost there ...",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+            text = "Your payment is pending",
+            color = Color.Black,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+        )
+    }
 }
